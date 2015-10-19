e5b0967bf8ef4915937897c7257fe6f72f5fe883
Nitramtj <nitramtj@gmail.com>
2015-10-19 11:13:36 -0500

    Minor experimental changes

--- src/goodybag.js
+++ src/goodybag.js
@@ -2,14 +2,26 @@ import ResourceResolver from './resourceresolver';
 import Dexie from 'dexie';
 
 var db = new Dexie('ResourceDatabase');
-db.version(1).stores({
-	resources: 'resource'
+db.version(2).stores({
+	resources: 'url, resource'
 });
 db.open();
 
 export default {
+	addResource: function(url) {
+		db.resources.add({
+			url: url,
+			resource: 'hello world'
+		});
+	},
 	loadResource: function(url) {
-		
+		db.resources.where('url').equals(url).first()
+			.then(function(result) {
+				debugger;
+			})
+			.catch(function(err) {
+				debugger;
+			});
 	},
 	
 	ResourceResolver: ResourceResolver
